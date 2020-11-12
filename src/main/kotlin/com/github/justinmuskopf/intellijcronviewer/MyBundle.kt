b6f6348c55ccdcf16c776171b9604fc8570f77b8
GitHub Action <action@github.com>
2020-11-12 21:59:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/justinmuskopf/intellijcronviewer/MyBundle.kt
+++ src/main/kotlin/com/github/justinmuskopf/intellijcronviewer/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.justinmuskopf.intellijcronviewer
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/justinmuskopf/intellijcronviewer/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.justinmuskopf.intellijcronviewer.services
+
+import com.github.justinmuskopf.intellijcronviewer.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
